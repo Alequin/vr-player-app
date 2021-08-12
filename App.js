3b4ad3571688e75aae0b215baab75173e942b7b4
Alequin <j-cox03@hotmail.com>
2021-08-12 22:55:58 +0100

    Cleaned up unneeded npm packages

--- App.js
+++ App.js
@@ -1,36 +1,20 @@
-import { DarkTheme, NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { ImageBackground } from "react-native";
 import "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { VideoPlayer } from "./src/video-player/video-player";
 
-const Stack = createStackNavigator();
+export const App = () => (
+  <>
+    <StatusBar hidden />
+    <VideoPlayer />
+  </>
+);
 
-const backgroundImage = require("./assets/images/background.jpg");
+const AppWithSafeArea = () => (
+  <SafeAreaProvider>
+    <App />
+  </SafeAreaProvider>
+);
 
-const App = () => {
-  return (
-    <SafeAreaProvider>
-      <StatusBar hidden />
-      <ImageBackground
-        source={backgroundImage}
-        style={{
-          height: "100%",
-          width: "100%",
-          resizeMode: "cover",
-        }}
-      >
-        <NavigationContainer theme={DarkTheme}>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Root" component={VideoPlayer} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </ImageBackground>
-    </SafeAreaProvider>
-  );
-};
-
-export default App;
+export default AppWithSafeArea;
